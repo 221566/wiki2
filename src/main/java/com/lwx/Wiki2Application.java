d12244eae0f4e69b70a6cfc69f5fe3b04c310d0a
221566 <2215668921@qq.com>
2022-09-22 22:49:52 +0800

    2-2 info

---
+++ src/main/java/com/lwx/Wiki2Application.java
@@ -0,0 +1,13 @@
+package com.lwx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Wiki2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Wiki2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/lwx/Wiki2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lwx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Wiki2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
