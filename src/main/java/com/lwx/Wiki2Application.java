5f5b36025e4700df6868f1f0148312d713a2f03a
221566 <2215668921@qq.com>
2022-09-23 15:09:58 +0800

    2-5 修改启动文案

--- src/main/java/com/lwx/Wiki2Application.java
+++ src/main/java/com/lwx/Wiki2Application.java
@@ -1,13 +1,18 @@
 package com.lwx;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class Wiki2Application {
-
+    private static final Logger LOG = LoggerFactory.getLogger(Wiki2Application.class);
     public static void main(String[] args) {
-        SpringApplication.run(Wiki2Application.class, args);
+        SpringApplication app = new SpringApplication(Wiki2Application.class);
+        Environment env = app.run(args).getEnvironment();
+        LOG.info("启动成功！！");
+        LOG.info("地址:\thttp://127.0.0.1:{}",env.getProperty("server.port"));
     }
-
 }
