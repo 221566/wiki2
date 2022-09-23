4388481aafa5b944a86116e90f7d4d539862e23c
221566 <2215668921@qq.com>
2022-09-23 15:44:05 +0800

    2-7 增加了hello接口测试test.http

--- src/main/java/com/lwx/controller/TestController.java
+++ src/main/java/com/lwx/controller/TestController.java
@@ -1,14 +1,19 @@
 package com.lwx.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
 
-    @RequestMapping("/hello")
+    @GetMapping("/hello")
     public String hello(){
         return "Hello World";
+    }
+
+    @PostMapping("/hello/post")
+    public String helloPost(String name){
+        return "Hello World POST," + name;
     }
 }
