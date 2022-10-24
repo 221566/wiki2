b7adefed8da94afae66247f3775627a367a64c28
221566 <2215668921@qq.com>
2022-10-24 10:23:44 +0800

    解除使用RocketMQ解耦通知功能，使用Spring自带对的异步解耦

--- src/main/java/com/lwx/service/DocService.java
+++ src/main/java/com/lwx/service/DocService.java
@@ -17,7 +17,6 @@ import com.lwx.util.CopyUtil;
 import com.lwx.util.RedisUtil;
 import com.lwx.util.RequestContext;
 import com.lwx.util.SnowFlake;
-import org.apache.rocketmq.spring.core.RocketMQTemplate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
@@ -50,8 +49,8 @@ public class DocService {
     @Resource
     public WsService wsService;
 
-    @Resource
-    private RocketMQTemplate rocketMQTemplate;
+//    @Resource
+//    private RocketMQTemplate rocketMQTemplate;
 
     public List<DocQueryResp> all(Long ebookId)  {
         DocExample docExample = new DocExample();
@@ -153,8 +152,8 @@ public class DocService {
         //推送消息
         Doc doc = docMapper.selectByPrimaryKey(id);
         String logId = MDC.get("LOG_ID");
-//        wsService.sendInfo("【"+doc.getName()+"】被点赞",logId);
-        rocketMQTemplate.convertAndSend("VOTE_TOPIC","【"+doc.getName()+"】被点赞");
+        wsService.sendInfo("【"+doc.getName()+"】被点赞",logId);
+//        rocketMQTemplate.convertAndSend("VOTE_TOPIC","【"+doc.getName()+"】被点赞");
     }
 
     public void updateEbookInfo(){
