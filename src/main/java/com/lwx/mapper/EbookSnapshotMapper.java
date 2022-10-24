91ecda7c550b6a91d87c718fba95a40288ba317a
221566 <2215668921@qq.com>
2022-10-24 17:11:14 +0800

    增加定时任务定时收集数据 要在xml一次性执行多个sql 需要为数据库连接加上参数：allowMultiQueries=true

--- src/main/java/com/lwx/mapper/EbookSnapshotMapper.java
+++ src/main/java/com/lwx/mapper/EbookSnapshotMapper.java
@@ -2,8 +2,9 @@ package com.lwx.mapper;
 
 import com.lwx.domain.EbookSnapshot;
 import com.lwx.domain.EbookSnapshotExample;
-import java.util.List;
 import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 public interface EbookSnapshotMapper {
     long countByExample(EbookSnapshotExample example);
@@ -27,4 +28,6 @@ public interface EbookSnapshotMapper {
     int updateByPrimaryKeySelective(EbookSnapshot record);
 
     int updateByPrimaryKey(EbookSnapshot record);
+
+    public void genSnapshot();
 }
---
+++ src/main/java/com/lwx/service/EbookSnapshotService.java
@@ -0,0 +1,33 @@
+package com.lwx.service;
+
+
+import com.lwx.mapper.EbookSnapshotMapper;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+@Service
+public class EbookSnapshotService {
+
+    @Resource
+    private EbookSnapshotMapper EbookSnapshotMapper;
+
+    public void genSnapshot() {
+        EbookSnapshotMapper.genSnapshot();
+    }
+
+    /**
+     * 获取首页数值数据：总阅读数、总点赞数、今日阅读数、今日点赞数、今日预计阅读数、今日预计阅读增长
+     */
+//    public List<StatisticResp> getStatistic() {
+//        return ebookSnapshotMapperCust.getStatistic();
+//    }
+
+    /**
+     * 30天数值统计
+     */
+//    public List<StatisticResp> get30Statistic() {
+//        return ebookSnapshotMapperCust.get30Statistic();
+//    }
+
+}
