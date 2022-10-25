3d3e33ba24169af090fff1b8b6efae6b5731ddab
221566 <2215668921@qq.com>
2022-10-25 16:22:03 +0800

    集成echarts5.0.2增加示例

--- src/main/java/com/lwx/controller/EbookSnapshotController.java
+++ src/main/java/com/lwx/controller/EbookSnapshotController.java
@@ -3,15 +3,15 @@ package com.lwx.controller;
 import com.lwx.resp.CommonResp;
 import com.lwx.resp.StatisticResp;
 import com.lwx.service.EbookSnapshotService;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 import java.util.List;
 
-@Controller
-@RequestMapping("EbookSnapshot")
+@RestController
+@RequestMapping("/ebookSnapshot")
 public class EbookSnapshotController {
 
     @Resource
--- web/src/components/the-welcome.vue
+++ web/src/components/the-welcome.vue
@@ -88,13 +88,20 @@
                 </a-card>
             </a-col>
         </a-row>
-
+        <br>
+        <a-row>
+            <a-col :span="24" id="main-col">
+                <div id="main" style="width: 100%;height:300px;"></div>
+            </a-col>
+        </a-row>
     </div>
 </template>
 
 <script lang="ts">
     import {defineComponent,ref, onMounted} from 'vue';
     import axios from 'axios';
+
+    declare let echarts: any;
 
     export default defineComponent({
         name: 'the-welcome',
@@ -103,7 +110,7 @@
             const statistic = ref();
             statistic.value = {};
             const getStatistic = () => {
-                axios.get('/EbookSnapshot/getStatistic').then((response) => {
+                axios.get('/ebookSnapshot/getStatistic').then((response) => {
                     /**
                      * 取到值后赋给statisticResp这个变量，这个变量是个数值接口返回的数据是昨天和今天所以这是个数值
                      * 接口返回数组[昨天，今天]所以昨天是第0条今天是第一条
@@ -134,12 +141,43 @@
                 });
             };
 
-            onMounted(() =>{
+            const testEcharts = () => {
+                // 基于准备好的dom，初始化echarts实例
+                //echarts是全局变量但是他是属于VUE外部的这里要声明下：declare let echarts: any;
+                const myChart = echarts.init(document.getElementById('main'));
+
+                // 指定图表的配置项和数据
+                const option = {
+                    title: {
+                        text: 'ECharts 入门示例'
+                    },
+                    tooltip: {},
+                    legend: {
+                        data:['销量']
+                    },
+                    xAxis: {
+                        data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]
+                    },
+                    yAxis: {},
+                    series: [{
+                        name: '销量',
+                        type: 'bar',
+                        data: [5, 20, 36, 10, 10, 20]
+                    }]
+                };
+
+                // 使用刚指定的配置项和数据显示图表。
+                myChart.setOption(option);
+            };
+
+            onMounted(() => {
                 getStatistic();
+                testEcharts();
+                // get30DayStatistic();
             });
 
             return {
-
+                statistic
             }
         }
     });
